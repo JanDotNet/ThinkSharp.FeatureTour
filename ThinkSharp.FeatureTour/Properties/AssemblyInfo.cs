74686c77bc1e19c6c4dd8994ef7020954dc43a70
JanDotNet <Jan-NiklasSchaefer@gmx.de>
2017-09-06 12:53:03 +0200

    improved title in nuget specification and AssembyInfo

--- ThinkSharp.FeatureTour/Properties/AssemblyInfo.cs
+++ ThinkSharp.FeatureTour/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FeatureTour - WPF touring component")]
+[assembly: AssemblyTitle("FeatureTour - Enhance your WPF applications with interactive Tutorials!")]
 [assembly: AssemblyDescription("Component for creating interactive tours for WPF applications.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ThinkSharp")]
